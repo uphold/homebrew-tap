cdec00ad3bd5f2891bdb73ba9f9c9fead99fda34
Alex Disney <alex.disney@uphold.com>
2020-06-30 18:49:24 -0500

    Add gsts v2.3.0

---
+++ Formula/gsts.rb
@@ -0,0 +1,30 @@
+require "language/node"
+
+class Gsts < Formula
+  desc "Obtain and store AWS STS credentials by authenticating via G Suite SAML"
+  homepage "https://github.com/ruimarinho/gsts"
+  url "https://github.com/ruimarinho/gsts/archive/v2.3.0.tar.gz"
+  sha256 "17445e969c911c307118dd004e2a427fc352376f633857efa4665afc89a67c47"
+
+  bottle :unneeded
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test gsts`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
